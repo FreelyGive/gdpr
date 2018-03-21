ebfb00887b2d5ecdcd2f40e6f3feea042e287f6e
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-03-21 15:44:44 +0000

    Ensure collapsing works with other themes

--- modules/gdpr_consent/js/gdpr_consent.js
+++ modules/gdpr_consent/js/gdpr_consent.js
@@ -1,14 +1,25 @@
 (function ($) {
     $(function () {
         // Hide the description for any GDPR checkboxes.
-        var container = $('.gdpr_consent_agreement').parent().parent();
+        var container = $('.gdpr_consent_agreement').parent();
         var desc = container.next('.description');
+
+        if(!desc.length) {
+            container = container.parent();
+            desc = container.next('.description');
+        }
+
         desc.hide();
 
         $('<a href="javascript:void(0)" class="gdpr_agreed_toggle">?</a>')
             .appendTo(container)
             .click(function () {
-                $(this).parent().next('.description').slideToggle()
+                var desc = $(this).next('.description');
+                if(!desc.length) {
+                    desc = $(this).parent().next('.description');
+                }
+
+                desc.slideToggle()
             });
 
         // Do the same for implicit
