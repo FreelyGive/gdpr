b9e2fda9cd1f951156e8ea0988658bd9a3a20c85
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-03-27 10:55:47 +0100

    Tidy up

--- modules/gdpr_fields/src/Entity/GdprFieldConfigEntity.php
+++ modules/gdpr_fields/src/Entity/GdprFieldConfigEntity.php
@@ -26,11 +26,39 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
  */
 class GdprFieldConfigEntity extends ConfigEntityBase {
 
+  /**
+   * Associative array.
+   *
+   * Each element is keyed by bundle name and contains an array representing
+   * a list of fields.
+   *
+   * Each field is in turn represented as a nested array.
+   *
+   * @var array
+   */
   public $bundles = [];
 
+  /**
+   * Sets a GDPR field's settings.
+   *
+   * @param string $bundle
+   *   Bundle.
+   * @param string $field_name
+   *   Field.
+   * @param bool $enabled
+   *   Enabled.
+   * @param string $rta
+   *   RTA setting.
+   * @param string $rtf
+   *   RTF setting.
+   * @param string $sanitizer
+   *   Sanitizer to use.
+   * @param string $notes
+   *   Notes.
+   *
+   * @return $this
+   */
   public function setField($bundle, $field_name, $enabled, $rta, $rtf, $sanitizer, $notes) {
-
-
     $this->bundles[$bundle][$field_name] = [
       'bundle' => $bundle,
       'name' => $field_name,
