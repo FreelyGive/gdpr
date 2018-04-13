6385e69f4f59e70509d18f81a9bffc2a915180ab
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2018-04-13 15:24:53 +0000

    By yanniboi: GDPR Dump module with data sanitizers.

---
+++ modules/gdpr_dump/plugins/gdpr_sanitizer/GDPRSanitizerName.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Base class for export UI.
+ */
+class GDPRSanitizerName extends GDPRSanitizerDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  var $name = 'gdpr_sanitizer_name';
+
+  /**
+   * {@inheritdoc}
+   */
+  var $label = 'Name field sanitizer';
+
+  /**
+   * Constant for name length.
+   */
+  const MIN_LENGTH = 4;
+
+  /**
+   * Constant for name length.
+   */
+  const MAX_LENGTH = 12;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function sanitize($input, $field = NULL) {
+    if (empty($input)) {
+      return $input;
+    }
+
+    $random = new GDPRUtilRandom();
+    return array(
+      'given' => $random->word(rand(self::MIN_LENGTH, self::MAX_LENGTH)),
+      'family' => $random->word(rand(self::MIN_LENGTH, self::MAX_LENGTH)),
+    );
+  }
+}
