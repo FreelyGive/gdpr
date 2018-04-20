ebf1552dcfb898ff92fc5617d882d073322798dd
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2018-04-20 11:02:27 +0000

    By yanniboi: Fix field list with fieldsets.

--- modules/gdpr_fields/plugins/export_ui/gdpr_fields_ui.class.php
+++ modules/gdpr_fields/plugins/export_ui/gdpr_fields_ui.class.php
@@ -28,47 +28,70 @@ class gdpr_fields_ui extends ctools_export_ui {
    * @param GDPRFieldData $item
    */
   public function list_build_row($item, &$form_state, $operations) {
-    parent::list_build_row($item, $form_state, $operations);
-
+    // Set up sorting
     $name = $item->{$this->plugin['export']['key']};
-    $row = $this->rows[$name];
-    unset($this->rows[$name]);
+    $schema = ctools_export_get_schema($this->plugin['schema']);
 
-    $ops = array_pop($row['data']);
+    // Note: $item->{$schema['export']['export type string']} should have already been set up by export.inc so
+    // we can use it safely.
+    switch ($form_state['values']['order']) {
+      case 'disabled':
+        $this->sorts[$name] = empty($item->disabled) . $name;
+        break;
+      case 'title':
+        $this->sorts[$name] = $item->{$this->plugin['export']['admin_title']};
+        break;
+      case 'name':
+        $this->sorts[$name] = $name;
+        break;
+      case 'storage':
+        $this->sorts[$name] = $item->{$schema['export']['export type string']} . $name;
+        break;
+    }
 
-    $title = array('data' => check_plain($item->getSetting('label')), 'class' => array('ctools-export-ui-title'));
-    array_unshift($row['data'], $title);
+    $row['data'] = array();
+    $row['class'] = !empty($item->disabled) ? array('ctools-export-ui-disabled') : array('ctools-export-ui-enabled');
 
-    unset($row['data'][1]);
+    // If we have an admin title, make it the first row.
+    $row['data'][] = array('data' => check_plain($item->getSetting('label')), 'class' => array('ctools-export-ui-title'));
+//    $row['data'][] = array('data' => check_plain($name), 'class' => array('ctools-export-ui-name'));
+    $row['data'][] = array('data' => check_plain($item->{$schema['export']['export type string']}), 'class' => array('ctools-export-ui-storage'));
 
 //    $row['data'][] = array('data' => check_plain($item->entity_type), 'class' => array('ctools-export-ui-entity-type'));
 //    $row['data'][] = array('data' => check_plain($item->entity_bundle), 'class' => array('ctools-export-ui-entity-bundle'));
 //    $row['data'][] = array('data' => check_plain($item->field_name), 'class' => array('ctools-export-ui-field-name'));
     $row['data'][] = array('data' => check_plain($item->getSetting('gdpr_fields_rta', 'none')), 'class' => array('ctools-export-ui-rta'));
     $row['data'][] = array('data' => check_plain($item->getSetting('gdpr_fields_rtf', 'none')), 'class' => array('ctools-export-ui-rtf'));
-    $row['data'][] = $ops;
 
-//    $this->rows[$item->entity_type][$item->entity_bundle][$name] = $row;
+    $ops = theme('links__ctools_dropbutton', array('links' => $operations, 'attributes' => array('class' => array('links', 'inline'))));
+
+    $row['data'][] = array('data' => $ops, 'class' => array('ctools-export-ui-operations'));
+
+    // Add an automatic mouseover of the description if one exists.
+    if (!empty($this->plugin['export']['admin_description'])) {
+      $row['title'] = $item->{$this->plugin['export']['admin_description']};
+    }
+
+    $this->rows[$name] = $row;
   }
 
   /**
    * {@inheritdoc}
    */
   public function list_table_header() {
-    $header = parent::list_table_header();
-    $ops = array_pop($header);
+    $header = array();
+    $header[] = array('data' => t('Label'), 'class' => array('ctools-export-ui-title'));
 
-    $title = array('data' => t('Label'), 'class' => array('ctools-export-ui-title'));
-    array_unshift($header, $title);
-
-    unset($header[1]);
+//    $header[] = array('data' => t('Name'), 'class' => array('ctools-export-ui-name'));
+    $header[] = array('data' => t('Storage'), 'class' => array('ctools-export-ui-storage'));
 
 //    $header[] = array('data' => t('Entity type'), 'class' => array('ctools-export-ui-entity-type'));
 //    $header[] = array('data' => t('Entity bundle'), 'class' => array('ctools-export-ui-entity-bundle'));
 //    $header[] = array('data' => t('Field name'), 'class' => array('ctools-export-ui-field-name'));
     $header[] = array('data' => t('Right to access'), 'class' => array('ctools-export-ui-rta'));
     $header[] = array('data' => t('Right to be forgotten'), 'class' => array('ctools-export-ui-rtf'));
-    $header[] = $ops;
+
+    $header[] = array('data' => t('Operations'), 'class' => array('ctools-export-ui-operations'));
     return $header;
 
   }
@@ -77,10 +100,16 @@ class gdpr_fields_ui extends ctools_export_ui {
    * {@inheritdoc}
    */
   public function list_render(&$form_state) {
-    $tables = '';
+    $tables = array();
+    $table_data = '';
+    drupal_add_library('system', 'drupal.collapse');
 
-    foreach ($this->rows as $entity_type => $entities) {
-      dpm($entity_type);
+    foreach ($this->rows as $name => $row) {
+      list($entity_type, $entity_bundle, $field_name) = explode('|', $name);
+      $tables[$entity_type][$entity_bundle][$name] = $row;
+    }
+
+    foreach ($tables as $entity_type => $entities) {
       foreach ($entities as $bundle => $rows) {
         $table = array(
           'header' => $this->list_table_header(),
@@ -88,13 +117,30 @@ class gdpr_fields_ui extends ctools_export_ui {
           'attributes' => array('id' => 'ctools-export-ui-list-items'),
           'empty' => $this->plugin['strings']['message']['no items'],
         );
-        $tables .= theme('table', $table);
+
+        $fieldset_vars = array(
+          'element' => array(
+            '#title' => t('@entity: @bundle', array(
+              '@entity' => $entity_type,
+              '@bundle' => $bundle,
+            )),
+            '#value' => theme('table', $table),
+            '#children' => '<div>',
+            '#attributes' => array (
+              'class' => array(
+                'collapsible',
+//                'collapsed',
+              ),
+            ),
+          ),
+        );
+
+        $table_data .= theme('fieldset', $fieldset_vars);
       }
     }
 
 
-    return '';
-    return $tables;
+    return $table_data;
   }
 
 }
